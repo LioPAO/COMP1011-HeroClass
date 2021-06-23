491341c953ce4f2d0cd8e21715331f6c96804e64
LioPAO <86293610+LioPAO@users.noreply.github.com>
2021-06-23 21:10:20 +0800

    Creating base template for Hero Class

---
+++ src/Lesson3HeroClass/Hero.java
@@ -0,0 +1,30 @@
+package Lesson3HeroClass;
+
+public class Hero {
+
+    //ATTRIBUTES -----------------------------------------------
+    private int strengh;
+    private int speed;
+    private int health;
+    private String name;
+
+    //PUBLIC METHODS -------------------------------------------
+
+    public void generateAbilitiies(){
+
+    }
+    public void fight(){
+
+    }
+    public void hitAttempt(){
+
+    }
+    public void hitDamge(){
+
+    }
+    public void show(){
+
+    }
+
+
+}
