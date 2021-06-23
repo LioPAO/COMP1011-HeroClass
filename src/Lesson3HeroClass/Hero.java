b36c8650f4d5d51f12b097eab7beea90adcb1da8
LioPAO <86293610+LioPAO@users.noreply.github.com>
2021-06-23 22:19:39 +0800

    Documenting the project.

--- src/Lesson3HeroClass/Hero.java
+++ src/Lesson3HeroClass/Hero.java
@@ -1,4 +1,14 @@
 package Lesson3HeroClass;
+/**
+ * Creating a Hero Class which creates an object of type Hero
+ * Heroes are created with a name given by the user
+ * The Hero's abilities (Strength, Speed, Health) are randomly generated upon creation.
+ * A hero has the ability to fight and cause damage.
+ * During a fight a hit attempt occurs 20% of the time
+ * If true, the damage is calculated by multiplying the Hero's strength by a random number from 1-6
+ * Once a hit is done, the damage is displayed else nothing.
+ * Finally the Hero's ability stats can be shown using the show method.
+ */
 
 public class Hero {
 
@@ -24,16 +34,16 @@ public class Hero {
     //PUBLIC METHODS -------------------------------------------
     public void fight(){
        if( hitAttempt()) {
-           System.out.println(this.name + " has received " + hitDamge() + " damage.");
+           System.out.println(this.name + " has caused " + hitDamage() + " damage.");
        }else {
-           System.out.println(this.name + " has received " + "0 damage.");
+           System.out.println(this.name + " has not hit.");
        }
     }
     public boolean hitAttempt(){
         int random = (int) (Math.random() * 101);
         return (random < 21);
     }
-    public int hitDamge(){
+    public int hitDamage(){
         int random = (int )(Math.random()*6)+1;
         return random * this.strength;
     }
